51669ee1fc359f17d8ec0b37f5ed65204fca2cb7
kayqueproenca <kayqueproencademacedo@gmail.com>
2025-02-15 16:47:31 +0000

    Alteração no incluir_produto, verifica se já tem algum item com o ID digitado pelo usuário

--- incluir_produto.sh
+++ incluir_produto.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
 source variaveis_gerais
 
+#Descomente a linha abaixo caso queira debugar o códiogo
+#set -x
+
 #Inicio da função para coleta de dados.
 coleta_info_prod(){
  COUNT=0
+ DISPONIVEL=$( mariadb -u$DBUSER -p$DBPASS -h$DBHOST $BASE -Be "SELECT ID FROM produto ORDER BY ID DESC LIMIT 1;" | awk -F"\t" 'NR!=1{print $1}')
  while : ; do
   while true; do
    ID[$COUNT]=""
@@ -12,13 +16,25 @@ coleta_info_prod(){
    ESTOQUE[$COUNT]=""
    PRECO[$COUNT]=""
    DESC[$COUNT]=""
+   DUPLICADA=""
    clear
    echo $DELIMITADOR
    echo "Digite as informações do produto"
    echo $DELIMITADOR
    while ! [[ ${ID[$COUNT]} =~ ^[0-9]+$ ]]; do
-    read -p "Informe o ID do produto -> " ID[$COUNT]
+    read -p "Informe o ID do produto (somente números) -> " ID[$COUNT]
    done
+   DUPLICADA=$( mariadb -u$DBUSER -p$DBPASS -h$DBHOST $BASE -Be "SELECT COUNT(*) FROM produto WHERE ID = ${ID[$COUNT]}" | awk -F"\t" 'NR!=1{print $1}')
+   if [ $DUPLICADA -gt 0 ] ; then
+   while [ $DUPLICADA -gt 0 ] ;do
+    echo -e "${RED_BOLD}Já tem um item com ID ${ID[$COUNT]}${END_COLOR}\n${WHITE_BOLD}Dica:${END_COLOR}${BLUE_BOLD} Use um ID ACIMA de ${DISPONIVEL}${END_COLOR}"
+    ID[$COUNT]=""
+    while ! [[ ${ID[$COUNT]} =~ ^[0-9]+$ ]]; do
+     read -p "Informe o ID do produto (somente números) -> " ID[$COUNT]
+     DUPLICADA=$( mariadb -u$DBUSER -p$DBPASS -h$DBHOST $BASE -Be "SELECT COUNT(*) FROM produto WHERE ID = ${ID[$COUNT]}" 2> /dev/null | awk -F"\t" 'NR!=1{print $1}')
+    done
+   done
+   fi
    while [ -z "${NOME[$COUNT]}" ] ; do
     read -p "Informe o NOME do produto -> " NOME[$COUNT]
    done
@@ -83,6 +99,7 @@ coleta_info_prod(){
      break
     else
      let COUNT++
+     let DISPONIVEL++
      continue
     fi
  done
@@ -101,7 +118,7 @@ fi
 for (( i=0 ; i <= $COUNT ; i++ )) ; do
  if \
 mysql -u$DBUSER -p$DBPASS -h$DBHOST $BASE << EOF 2>> erro.log
-insert into produto (ID,NOME, MARCA,ESTOQUE, PRECO, DESCRICAO) values (${ID[$i]}, '${NOME[$i]}','${MARCA[$i]}', ${ESTOQUE[$i]}, ${PRECO[$i]}, '${DESC[$i]}');
+insert into produto (ID,NOME, MARCA,ESTOQUE, PRECO, DESCRICAO) values (${ID[$i]}, '${NOME[$i]^^}','${MARCA[$i]^^}', ${ESTOQUE[$i]}, ${PRECO[$i]}, '${DESC[$i]^^}');
 EOF
  then
   echo $DELIMITADOR

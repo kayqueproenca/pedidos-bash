ddf96707c745b92d05140fe482b5224a7f848288
kayqueproenca <kayqueproencademacedo@gmail.com>
2025-05-20 02:24:45 +0000

    Feito melhorias na busca do produto, caso não esteja no banco, vamos ter mensagem de aviso

--- pedido_novo.sh
+++ pedido_novo.sh
@@ -9,6 +9,7 @@ source ./variaveis_gerais
 cliente_registrado(){
  CLIENTE=""
  CLIENTE_PEDIDO=""
+ DESCONTO=0
  local AVISO=""
  local COUNT=0
  local LISTA_CLIENTE=()
@@ -24,8 +25,9 @@ cliente_registrado(){
      CLIENTE=""
      echo $DELIMITADOR
      echo -e $AVISO
-      read -p "Informe o NOME/CPF do cliente: " CLIENTE
+      read -p "Informe o NOME/CPF do cliente: " CLIENTE #Neste campo, podemos por o nome, ou o CPF do cliente, caso exista, vamos ter um retorno, se for informado o nome, vamos ter uma lista de clientes, se tiver mais de um, o CPF, por ser único, mostra de forma direta
       AVISO=""
+      #Aqui temos um procedimento que vai verificar se o valor buscado, seja nome ou CPF, existe de fato lá no banco, com isso podemos prosseguir, caso tivermos um retorno maior que zero, caso contrário, vamos ter uma mensagem de erro, informando que o valor buscado não tem registro, e vai pedir para por um outro valor.
       if [[ $CLIENTE =~ ^[a-z|A-Z] ]] ;then
         QNT_RESULTADO=$( $CONEXAO -Be "SELECT COUNT(NOME_CLIENTE) FROM cliente WHERE NOME_CLIENTE REGEXP '^$CLIENTE';" 2> /dev/null | awk -F "\n" 'NR!=1 {print $1}')
       elif [[ $CLIENTE =~ ^[0-9]+$ ]];then
@@ -37,8 +39,10 @@ cliente_registrado(){
          AVISO="${RED_BOLD}Nenhum resultado encontrado para:${END_COLOR} ${CLIENTE}"
        fi
      done
-     if [[ $CLIENTE =~ ^[a-z|A-Z] ]]; then
-       mapfile -t LISTA_CLIENTE< <($CONEXAO -Be "SELECT NOME_CLIENTE FROM cliente WHERE NOME_CLIENTE REGEXP '^$CLIENTE';" | awk -F "\n" 'NR!=1 {print $1}')
+     #Fim do procedimento.
+
+     if [[ $CLIENTE =~ ^[a-z|A-Z] ]]; then #Caso a busca seja feita por nome, vamos ter uma expressão REGEX, que vai fazer a busca, e vamos ter uma lista com possíveis nome como retorno, podemos ter mais de um registro, tudo depende de como a busca foi feita.
+       mapfile -t LISTA_CLIENTE< <($CONEXAO -Be "SELECT NOME_CLIENTE FROM cliente WHERE NOME_CLIENTE REGEXP '^$CLIENTE';" | awk -F "\n" 'NR!=1 {print $1}') #Por padrão o retorno é linhas, o mapfile pega essas linhas, e coloca em um array, que vamos poder usar no select, trata-se de um comando built-in :-)
          clear
          echo -e "Segue o resultado para o ${BLUE_BOLD}${CLIENTE^^}${END_COLOR}"
          select LISTA in "${LISTA_CLIENTE[@]}"; do
@@ -50,7 +54,7 @@ cliente_registrado(){
          echo "Resultado da busca:"
          $CONEXAO -Be "SELECT CPF, NOME_CLIENTE, FIDELIDADE FROM cliente WHERE NOME_CLIENTE = '$CLIENTE';" | awk -F "\t" 'NR!=1 {print "CPF: "$1"\nNOME: "$2"\nFIDELIDADE: "$3}'
          CLIENTE_PEDIDO=$($CONEXAO -Be "Select CPF FROM cliente WHERE NOME_CLIENTE = '$CLIENTE';" | awk -F "\t" 'NR!=1 {print $1}')
-         DESCONTO=$($CONEXAO -Be "Select DESCONTO FROM cliente WHERE CPF = $CLIENTE_PEDIDO;" | awk -F"\t" 'NR!=1 {print $1}')
+         DESCONTO=$($CONEXAO -Be "Select DESCONTO FROM cliente WHERE CPF = $CLIENTE_PEDIDO;" | awk -F"\t" 'NR!=1 {print $1}') #Aqui, vamos pegar o valor que temos na coluna "desconto" que temos no banco, temos que por esse valor em uma variavel propria, já que vamos utilizar esse valor lá na frente.
          break
      else
        $CONEXAO -Be "SELECT CPF, NOME_CLIENTE, FIDELIDADE FROM cliente WHERE CPF = $CLIENTE;" | awk -F "\t" 'NR!=1 {print "CPF: "$1"\nNOME: "$2"\nFIDELIDADE: "$3}'
@@ -82,6 +86,8 @@ cliente_registrado(){
     clear
     echo "Cancelando pedido..."
     CLIENTE_PEDIDO=""
+    QNT_RESULTADO=0
+    CLIENTE=""
     break
    fi
   done
@@ -96,11 +102,11 @@ cliente_registrado(){
 
 #EXCLUSIVA PARA SOMENTE MOSTRAR O CLIENTE QUE FOI CONSULTADO
 mostrar_cliente(){
- if [ $1 -eq 11111111111 ] ; then
+ if [ $1 -eq 11111111111 ] ; then #Aqui, caso o cliente não seja cadastrado, vamos por um número genérico, que não vai ser usado, como CPF, e ele vai ser registrado no banco, e vai informar que o cliente em questão não existe, e não vai desconto.
   echo -e "Compra com cliente não cadastrado\n${RED_BOLD}SEM DIREITO A DESCONTO FIDELIDADE${END_COLOR}"
  else
   $CONEXAO -Be "SELECT CPF, NOME_CLIENTE, FIDELIDADE FROM cliente WHERE CPF = $1;" | awk -F "\t" 'NR!=1 {print "CPF: "$1"\nNOME: "$2"\nFIDELIDADE: "$3}'
-  echo -e "Desconto concedido ${BLUE_BOLD}${DESCONTO}%${END_COLOR}"
+  echo -e "Desconto concedido ${BLUE_BOLD}${DESCONTO}0%${END_COLOR}"
  fi
 }
 
@@ -120,18 +126,33 @@ lista_itens(){
   while : ; do
    local ITEM=""
    local QUANTIDADE=""
-   while [ -z $ITEM ] ; do
+   local AVISO=""
+   local QNT_ITEM=0
+   while [ $QNT_ITEM -eq 0 ] ; do
     clear
     echo $DELIMITADOR
     mostrar_cliente $CLIENTE_PEDIDO
     echo $DELIMITADOR
     echo -e "\nLista de itens:"
     mostrar_produto
-    echo -e "\n
-    Subtotal: R\$ ${SUBTOTAL}"
-    echo "Coloque o CÓDIGO/NOME do produto:"
-    read -p "-> " ITEM
-   done 
+    echo -e "\nSubtotal: R\$ ${SUBTOTAL}\n"
+    if [ $DESCONTO -gt 0 ] ; then
+     echo -e "Subtotal com desconto de ${DESCONTO}0%: R\$ ${SUBTOTAL_DESC}\n"
+    fi
+    #while [ $QNT_ITEM -eq 0 ] ; do
+     echo -e $AVISO
+     read -p "Coloque o CÓDIGO/NOME do produto: " ITEM
+     if [[ $ITEM =~ ^[A-Z|a-z] ]] ; then
+      QNT_ITEM=$($CONEXAO -Be "SELECT COUNT(ID) FROM produto WHERE NOME REGEXP '^$ITEM' ;" | awk -F "\n" 'NR!=1{print $1}')
+     elif [[ $ITEM  =~ ^[0-9]+$ ]] ; then
+      QNT_ITEM=$($CONEXAO -Be "SELECT COUNT(ID) FROM produto WHERE ID = $ITEM ;" | awk -F "\n" 'NR!=1{print $1}')
+     else
+      AVISO="O campo não pode ficar em branco"
+     fi
+     if [ $QNT_ITEM -eq 0 ] ; then
+      AVISO="${RED_BOLD}NENHUM RESULTADO PARA O ITEM:${END_COLOR} $ITEM"
+     fi
+    done
    if [[ $ITEM =~ ^[A-Z|a-z] ]] ; then
     readarray -t LISTA< <($CONEXAO -Be "SELECT DISTINCT NOME FROM produto WHERE NOME REGEXP '^$ITEM' ;" | awk -F "\n" 'NR!=1{print $1}')
     echo $DELIMITADOR
@@ -177,7 +198,12 @@ lista_itens(){
      CONFIRMA="sim"
      LISTA_ITEM[$COUNT_ITEM]=$ITEM
      QUANTIDADE_LISTA[$COUNT_ITEM]=$QUANTIDADE
-     SUBTOTAL=$( echo "$PRECO * $QUANTIDADE + $SUBTOTAL" | bc )
+     if [ $DESCONTO -eq 0 ]; then
+       SUBTOTAL=$( echo "$PRECO * $QUANTIDADE + $SUBTOTAL" | bc )
+     else
+       SUBTOTAL=$( echo "$PRECO * $QUANTIDADE + $SUBTOTAL" | bc )
+       SUBTOTAL_DESC=$(echo "$SUBTOTAL - ($SUBTOTAL * 0.$DESCONTO)" | bc)
+     fi
      break
     else
      CONFIRMA="nao"
@@ -220,11 +246,13 @@ confirmar_compra() {
  while : ; do
   clear
   mostrar_cliente $CLIENTE_PEDIDO
-  NUMERO_PEDIDO=$(date +%Y%m%d)
+  DATA_PEDIDO=$(date +%Y%m%d)
+  NUMERO_PEDIDO="${DATA_PEDIDO}${RANDOM}"
   echo -e "${BLUE_BOLD}PEDIDO Nº ${END_COLOR} $NUMERO_PEDIDO"
   echo -e "Lista de produtos:\n"
   mostrar_produto
   echo -e "\nSUBTOTAL R\$ $SUBTOTAL"
+  echo -e "SUBTOTAL COM DESCONTO: R\$ $SUBTOTAL_DESC"
   echo "Confirma pedido?"
   select OPC in "SIM" "NÃO" ; do
    if [ $REPLY -eq 1 ] ; then
@@ -245,4 +273,3 @@ confirmar_compra() {
 }
 
 cliente_registrado
-
